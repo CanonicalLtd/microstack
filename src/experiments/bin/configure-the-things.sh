7b4e61201bb3aa168f9f30820b232adbf83b9510
Pete Vander Giessen <pete.vandergiessen@canonical.com>
2019-06-25 13:45:00 -0400

    Cruft cleanup. (#75)
    * Cruft cleanup.
    
    Organized snapcraft.yaml better, and got rid of unecessary bits.
    
    * Cleaned up scripts.
    
    Moved launch.sh into snap-overlay, to be more consistent with
    everything else.
    
    Got rid of outdated and confusing configure-the-things script.
    
    * Clarified a comment.
    
    * Got rid of erroneous config file.
    
    * Undeleted config file -- wasn't erroneous!

--- src/experiments/bin/configure-the-things.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-/var/snap/microstack/common/bin/configure-the-things.sh
-
-exit 0
-
-microstack.mysql-client -u root -p$(`sudo cat /var/snap/microstack/common/log/error.log | grep "temporary password" | cut -d " " -f11`) --connect-expired-password | echo "ALTER USER 'root'@'10.20.20.1' IDENTIFIED BY 'fnord';"
